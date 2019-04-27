9202d9a8e7e6ba33855bc42fdda946349b422dc8
Alex Hortin <hortinstein@gmail.com>
2019-04-27 18:10:48 -0400

    Update build_rust.sh

--- build_rust.sh
+++ build_rust.sh
@@ -8,37 +8,37 @@ mkdir -p executables
 #build the instructor binaries
 git clone https://github.com/replicatedu/replicatedu_instructor.git
 cd replicatedu_instructor
-cargo build --release 
-cp target/release/replicatedu_instructor ../executables
+cargo build 
+cp target/debug/replicatedu_instructor ../executables
 cd ..
 
 #build the student binaries
 git clone https://github.com/replicatedu/replicatedu_student.git
 cd replicatedu_student
-cargo build --release 
-cp target/release/replicatedu_student ../executables
+cargo build  
+cp target/debug/replicatedu_student ../executables
 cd ..
 
 #build the test runner
 git clone https://github.com/replicatedu/test_runner.git
 cd test_runner
-cargo build --release --target=x86_64-unknown-linux-musl
-cp target/x86_64-unknown-linux-musl/release/test_runner ../executables
+cargo build --target=x86_64-unknown-linux-musl
+cp target/x86_64-unknown-linux-musl/debug/test_runner ../executables
 cd ..
 
 #build the managment server
 git clone https://github.com/replicatedu/management_server.git
 cd management_server
-PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --target=x86_64-unknown-linux-musl
-cp target/x86_64-unknown-linux-musl/release/server ../executables
-cp target/x86_64-unknown-linux-musl/release/client ../executables
+PKG_CONFIG_ALLOW_CROSS=1 cargo build --target=x86_64-unknown-linux-musl
+cp target/x86_64-unknown-linux-musl/debug/server ../executables
+cp target/x86_64-unknown-linux-musl/debug/client ../executables
 cd ..
 
 #build the docker_wrapper
 git clone https://github.com/replicatedu/docker_wrapper.git
 cd docker_wrapper
-cargo build --release --target=x86_64-unknown-linux-musl
-cp target/x86_64-unknown-linux-musl/release/docker_wrapper ../executables
+cargo build --target=x86_64-unknown-linux-musl
+cp target/x86_64-unknown-linux-musl/debug/docker_wrapper ../executables
 cd ..
 
 cd ..
